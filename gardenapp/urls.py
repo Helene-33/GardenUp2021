b4bf9f57db88d87b815626ce58648177440bd803
Helene Liobard <heleneliobard@hotmail.com>
2021-06-03 16:26:55 -0700

    Plant view comit

--- gardenapp/urls.py
+++ gardenapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('plants/', views.plants, name='plants'),
 ]
--- gardenapp/views.py
+++ gardenapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Plant, PlantType, Tips
 
 # Create your views here.
 def index(request):
     return render(request, 'gardenapp/index.html')
+
+def plants(request):
+    plant_list=Plant.objects.all()
+    return render(request, 'gardenapp/plants.html', {'plant_list': plant_list})
+    
